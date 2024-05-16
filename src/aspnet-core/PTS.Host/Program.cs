9e22ee14e80decd8be9f4f68dfb554def0095d7f
tvminhmt <tvminh.mt@gmail.com>
2024-05-16 07:27:50 +0700

    Migrations

--- src/aspnet-core/PTS.Host/Program.cs
+++ src/aspnet-core/PTS.Host/Program.cs
@@ -1,7 +1,18 @@
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using PTS.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<ApplicationDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+//IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+//builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddControllers();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
