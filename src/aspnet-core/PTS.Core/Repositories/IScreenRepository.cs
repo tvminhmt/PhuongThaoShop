2adee45375e5adfb06fc18401e71cfafca881da9
anhpvph26707@fpt.edu.vn <anhpvph26707@fpt.edu.vn>
2024-06-14 07:24:19 +0700

    Screen

---
+++ src/aspnet-core/PTS.Core/Repositories/IScreenRepository.cs
@@ -0,0 +1,17 @@
+﻿using Abp.Application.Services.Dto;
+using PTS.Base.Application.Dto;
+using PTS.Core.Dto;
+using PTS.Domain.Entities;
+
+namespace PTS.Core.Repositories
+{
+    public interface IScreenRepository
+    {
+        Task<bool> Create(ScreenEntity obj);
+        Task<bool> Update(ScreenEntity obj);
+        Task<bool> Delete(int id);
+        Task<IEnumerable<ScreenEntity>> GetList();
+        Task<PagedResultDto<ScreenDto>> GetPagedAsync(PagedRequestDto request);
+        Task<ScreenEntity> GetById(int id);
+    }
+}
