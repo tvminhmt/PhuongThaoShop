78aabd9483f682c9fb69f568fcf44f4d770d94c8
tvminhmt <tvminhmt@gmail.com>
2024-06-19 23:25:42 +0700

    update

---
+++ src/aspnet-core/PTS.WebAPI/Controllers/ManufacturerController.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PTS.Shared.Dto;
+using PTS.Application.Dto;
+using PTS.Domain.Entities;
+using PTS.Application.Interfaces.Repositories;
+
+namespace PTS.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ManufacturerController : BaseController
+    {
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+        public ManufacturerController(IMapper mapper, IUnitOfWork unitOfWork)
+        {
+            _mapper = mapper;
+            _unitOfWork = unitOfWork;
+        }
+        [HttpGet("GetList")]
+        public async Task<IActionResult> GetList()
+        {
+            return Ok(await _unitOfWork._manufacturerRepository.GetList());
+        }
+        [HttpPost("GetPaged")]
+        public async Task<IActionResult> GetPaged(PagedRequestDto request)
+        {
+            return Ok(await _unitOfWork._manufacturerRepository.GetPagedAsync(request));
+        }
+        [HttpGet("GetById")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            return Ok(await _unitOfWork._manufacturerRepository.GetById(id));
+        }
+        [HttpPost("CreateOrUpdateAsync")]
+        public async Task<IActionResult> CreateOrUpdateAsync(ManufacturerDto objDto)
+        {
+            var obj = _mapper.Map<ManufacturerEntity>(objDto);
+            if (objDto.Id > 0)
+            {
+                return Ok(await _unitOfWork._manufacturerRepository.Update(obj));
+            }
+            else
+            {
+                return Ok(await _unitOfWork._manufacturerRepository.Create(obj));
+            }
+        }
+        [HttpPost("Delete")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            return Ok(await _unitOfWork._manufacturerRepository.Delete(id));
+        }
+    }
+}
