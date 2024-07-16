5725e5f69f2fb6b5f4e7fb48e49a034a88aceea2
vinh2306 <ntvinh2306@gmail.com>
2024-07-16 17:57:00 +0700

    identity

---
+++ src/aspnet-core/PTS.Application/Identity/MediaRouter.cs
@@ -0,0 +1,28 @@
+﻿using MTS.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTS.Domain.Entities.Identity
+{
+    public class MediaRouter : BaseAuditableEntity
+    {
+        public int Id { get; set; }
+        public string RouteName { get; set; }
+        public string RouteDesc { get; set; }
+        public string MediaDomain { get; set; }
+        public string RoutePrefix { get; set; }
+        public string RouteType { get; set; }
+        public string ProxyDomain { get; set; }
+        public string AwsEndPoint { get; set; }
+        public string AwsKey { get; set; }
+        public string AwsSecret { get; set; }
+        public string AwsRegion { get; set; }
+        public string AwsBucket { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime? CrDateTime { get; set; }
+
+    }
+}
---
+++ src/aspnet-core/PTS.Application/Identity/UserLog.cs
@@ -0,0 +1,21 @@
+﻿using MTS.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace MTS.Domain.Entities.Identity
+{
+    public class UserLog : BaseAuditableEntity
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string FromIP { get; set; }
+        public string UserAction { get; set; }
+        public string ActionStatus { get; set; }
+        public DateTime CrDateTime { get; set; }
+    }
+}
