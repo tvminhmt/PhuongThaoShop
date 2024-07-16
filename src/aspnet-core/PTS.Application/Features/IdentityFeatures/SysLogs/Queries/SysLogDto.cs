4eec095c5c566f840193cf9a8cd6a58d15e1b1cf
vinh2306 <ntvinh2306@gmail.com>
2024-07-16 18:07:09 +0700

    SysLog Query

---
+++ src/aspnet-core/PTS.Application/Features/IdentityFeatures/SysLogs/Queries/SysLogDto.cs
@@ -0,0 +1,16 @@
+﻿using MTS.Application.Common.Mappings;
+using MTS.Domain.Entities.Identity;
+
+namespace MTS.Application.Features.IdentityFeatures.SysLogs.Queries
+{
+	public class SysLogDto: IMapFrom<SysLog>
+	{
+		public int Id { get; set; }
+		public string Message { get; set; }
+		public string Level { get; set; }
+		public DateTime? TimeStamp { get; set; }
+		public string Exception { get; set; }
+		public string SourceContext { get; set; }
+		public string Application { get; set; }
+	}
+}
---
+++ src/aspnet-core/PTS.Application/Features/IdentityFeatures/SysLogs/Queries/SysLogGetPageDto.cs
@@ -0,0 +1,6 @@
+﻿namespace MTS.Application.Features.IdentityFeatures.SysLogs.Queries
+{
+	public class SysLogGetPageDto : SysLogDto
+	{
+	}
+}
