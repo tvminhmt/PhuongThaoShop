fd25845e132f5f5abcaf3cbf7e96efd4a9fc11d1
vinh2306 <ntvinh2306@gmail.com>
2024-07-16 18:07:40 +0700

    UserLog Query

---
+++ src/aspnet-core/PTS.Application/Features/IdentityFeatures/UserLogs/Queries/UserLogGetPageDto.cs
@@ -0,0 +1,14 @@
+﻿using MTS.Application.Common.Mappings;
+using MTS.Domain.Entities.Identity;
+
+namespace MTS.Application.Features.IdentityFeatures.UserLogs.Queries
+{
+    public class UserLogGetPageDto : IMapFrom<UserLog>
+    {
+        public string UserName { get; set; }
+        public string FromIP { get; set; }
+        public string UserAction { get; set; }
+        public string ActionStatus { get; set; }
+        public DateTime CrDateTime { get; set; }
+    }
+}
