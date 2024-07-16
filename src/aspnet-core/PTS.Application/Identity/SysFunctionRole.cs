d94f91d874353119b4965cad47625b395a119fa9
vinh2306 <ntvinh2306@gmail.com>
2024-07-16 18:00:18 +0700

    identity

---
+++ src/aspnet-core/PTS.Application/Identity/SysFunctionRole.cs
@@ -0,0 +1,10 @@
+﻿using MTS.Domain.Common;
+
+namespace MTS.Domain.Entities.Identity
+{
+    public class SysFunctionRole : BaseAuditableEntity
+    {
+        public int SysFunctionId { get; set; }
+        public int RoleId { get; set; }
+    }
+}
---
+++ src/aspnet-core/PTS.Application/Identity/UserRole.cs
@@ -0,0 +1,10 @@
+﻿using MTS.Domain.Common;
+
+namespace MTS.Domain.Entities.Identity
+{
+    public class UserRole : BaseAuditableEntity
+    {
+        public int UserId { get; set; }
+        public int RoleId { get; set; }
+    }
+}
