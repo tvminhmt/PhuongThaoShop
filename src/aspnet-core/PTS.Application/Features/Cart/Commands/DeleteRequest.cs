1dd21ce0fa4e02de69faa732b119b44697ab7dac
tvminhmt <tvminh.mt@gmail.com>
2024-07-29 16:28:42 +0700

    update

---
+++ src/aspnet-core/PTS.Application/Features/Cart/Commands/DeleteRequest.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using PTS.Application.Dto;
+using PTS.Domain.Entities;
+using PTS.Application.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTS.Application.Features.Cart.Commands
+{
+    public class DeleteCartDetailQuery : IRequest<ServiceResponse>
+    {
+        public int Id { get; set; }
+    }
+    public class DeleteCartDetailHandler : IRequestHandler<DeleteCartDetailQuery, ServiceResponse>
+    {
+        private readonly ICartDetailRepository _repository;
+        public DeleteCartDetailHandler(ICartDetailRepository repository)
+        {
+            _repository = repository;
+        }
+        public async Task<ServiceResponse> Handle(DeleteCartDetailQuery request, CancellationToken cancellationToken)
+        {
+
+            if (await _repository.Delete(request.Id))
+                return new ServiceResponse(true, "Xóa thành công");
+            return new ServiceResponse(false, "Xóa thất bại");
+
+        }
+    }
+}
---
+++ src/aspnet-core/PTS.Application/Features/Cart/Commands/UpdateQuantityCartItemRequest.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using PTS.Application.Dto;
+using PTS.Domain.Entities;
+using PTS.Application.Interfaces.Repositories;
+using PTS.Core.Services;
+
+namespace PTS.Application.Features.Cart.Commands
+{
+    public class UpdateQuantityCartItemQuery : IRequest<ServiceResponse>
+    {
+        public int Quantity { get; set; }
+        public int IdCartDetail { get; set; }
+    }
+    public class UpdateQuantityCartItemHandler : IRequestHandler<UpdateQuantityCartItemQuery, ServiceResponse>
+    {
+        private readonly ICartDetailRepository _repo;
+        public UpdateQuantityCartItemHandler(ICartDetailRepository repo)
+        {
+            _repo = repo;
+        }
+        public async Task<ServiceResponse> Handle(UpdateQuantityCartItemQuery request, CancellationToken cancellationToken)
+        {
+            CartDetailEntity cartDetail = new CartDetailEntity();
+            cartDetail.Id = request.IdCartDetail;
+            cartDetail.Quantity = request.Quantity;
+            if (await _repo.UpdateQuantity(cartDetail))
+            {
+                return new ServiceResponse(true, "Cập nhật hành công");
+            }
+            return new ServiceResponse(false, "Cập nhật thất bại");
+        }
+    }
+}
