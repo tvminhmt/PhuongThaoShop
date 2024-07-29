bf4b9c119455604d6d3f8134e3abed53113b3386
tvminhmt <tvminh.mt@gmail.com>
2024-07-29 15:48:52 +0700

    update

---
+++ src/aspnet-core/PTS.Application/Interfaces/Repositories/ICartRepository.cs
@@ -0,0 +1,16 @@
+﻿using PTS.Application.Dto;
+using PTS.Domain.Entities;
+
+namespace PTS.Application.Interfaces.Repositories
+{
+    public interface ICartRepository
+    {
+        Task<bool> Create(CartEntity obj);
+        Task<bool> Update(CartEntity obj);
+        Task<bool> Delete(int id);
+        Task<IEnumerable<CartEntity>> GetAll();
+        Task<CartEntity> GetCartById(int id);
+        Task<IEnumerable<CartItemDto>> GetCartItem(string UserName);
+        //  Task<IEnumerable<CartItemDto>> GetAllCarts();
+    }
+}
