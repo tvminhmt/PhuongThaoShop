cb9e0097c93935c1c39bf01e0fb3185ed8679da6
tvminhmt <tvminh.mt@gmail.com>
2024-07-29 16:28:03 +0700

    update

---
+++ src/aspnet-core/PTS.Application/Features/BillDetail/Commands/BillDetailDeleteCommand.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using MediatR;
+using PTS.Application.Common.Mappings;
+using PTS.Core.Enums;
+using PTS.Application.Interfaces.Repositories;
+using PTS.Domain.Entities;
+using PTS.Shared;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTS.Application.Features.BillDetail.Commands
+{
+	public record BillDetailDeleteCommand : IRequest<Result<int>>, IMapFrom<BillDetailEntity>
+	{
+		public int Id { get; set; }
+	}
+
+	internal class BillDetailDeleteCommandHandler : IRequestHandler<BillDetailDeleteCommand, Result<int>>
+	{
+		private readonly IUnitOfWork _unitOfWork;
+		private readonly IMapper _mapper;
+		public BillDetailDeleteCommandHandler(IUnitOfWork unitOfWork, IMapper mapper)
+		{
+			_unitOfWork = unitOfWork;
+			_mapper = mapper;
+		}
+		public async Task<Result<int>> Handle(BillDetailDeleteCommand command, CancellationToken cancellationToken)
+		{
+			try
+			{
+				var entity = await _unitOfWork.Repository<BillDetailEntity>().GetByIdAsync(command.Id);
+				if (entity == null)
+				{
+					return await Result<int>.FailureAsync($"Id <b>{command.Id}</b> không tồn tại ");
+				}
+				entity = _mapper.Map<BillDetailEntity>(command);
+				entity.Status =(int)StatusEnum.Delete;
+				await _unitOfWork.Repository<BillDetailEntity>().UpdateFieldsAsync(entity,
+					x => x.Status);
+				var result = await _unitOfWork.Save(cancellationToken);
+				return result > 0
+						? await Result<int>.SuccessAsync(entity.Id, "Cập nhật dữ liệu thành công.")
+						: await Result<int>.FailureAsync("Cập nhật dữ liệu không thành công.");
+			}
+			catch (Exception e)
+			{
+				throw;
+			}
+		}
+	}
+}
